282a1cf9147e7d82bed3bdbe03ee038a9a40d2f3
SondreB <sondre@outlook.com>
2022-11-30 03:25:38 +0100

    Remove old components not in use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AboutComponent } from './about/about.component';
-import { AnnouncementsComponent } from './announcements/announcements.component';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { RegisterPaymentComponent } from './register/payment.component';
 import { RegisterComponent } from './register/register.component';
-import { RegisterSignComponent } from './register/sign.component';
-import { RegisterVerifyComponent } from './register/verify.component';
 import { RegistryComponent } from './registry/registry.component';
 
 @NgModule({
@@ -22,14 +16,9 @@ import { RegistryComponent } from './registry/registry.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    FetchDataComponent,
     AboutComponent,
-    AnnouncementsComponent,
     RegistryComponent,
     RegisterComponent,
-    RegisterVerifyComponent,
-    RegisterSignComponent,
-    RegisterPaymentComponent,
   ],
   imports: [
     BrowserModule,
